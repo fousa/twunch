6d1b13bafb335890f6062d90cfcc4adc73ef4d0d
Jelle Vandebeeck <jelle@fousa.be>
2011-04-13 13:02:14 -0700

    Add README

---
+++ README.m
@@ -0,0 +1,27 @@
+TWUNCH IPHONE APP
+=================
+
+Look at all the twunches registered on [twunch.be](http://www.twunch.be/). You can subscribe to them with your twitter account and get a detailed overview of the map and other subscribers.
+
+BUILD
+-----
+
+If you want to build the app yourself, the only thing you'll have to do is add this twitter.plist file to your project, make sure to use your own twitter API keys.
+
+`<?xml version="1.0" encoding="UTF-8"?>`
+`<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">`
+`<plist version="1.0">`
+`    <dict>`
+`        <key>consumer_key</key>`
+`        <string>YOUR_KEY</string>`
+`        <key>consumer_secret</key>`
+`        <string>YOUR_SECRET</string>`
+`    </dict>`
+`</plist>`
+
+CREDITS
+-------
+
+Developed by [me](http://www.fousa.be/) under the [MIT]() license.
+
+App website can be found [here](http://fousa.github.com/twunch).
